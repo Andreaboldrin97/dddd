84135a73decd86e0aab8336d98d06f28c92fb966
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-09-29 15:36:50 +0200

    import router.js on front.js

--- resources/js/front.js
+++ resources/js/front.js
@@ -1,9 +1,18 @@
 window.Vue = require('vue');
+// IMPORTIAMO LE FUNZIONALITA' DI VUE-ROUTER
+import VueRouter from 'vue-router';
+
+//! INDICHIAMO L'UTILLIZZO DI VUE ROUTER
+Vue.use(VueRouter)
 
 // IMPORTIAMO IL COMPONENTE App
 import App from './views/App.vue';
+// IMPORTIAMO ROUTE.JS
+import router from './router'
+import Vue from 'vue';
 
 const app = new Vue({
     el: '#root',
-    render: h => h(App) // MOSTRIAMO APP ALL'AVIO DI VUE
+    render: h => h(App), // MOSTRIAMO APP ALL'AVIO DI VUE
+    router
 });
