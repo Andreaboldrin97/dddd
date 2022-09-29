6bbde260c5b144645846e5f69aef2d5b4cba1378
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-09-29 17:26:05 +0200

    fix singlePost center

--- public/js/front.js
+++ public/js/front.js
@@ -2305,7 +2305,9 @@ var render = function render() {
   var _vm = this,
       _c = _vm._self._c;
 
-  return _c("div", [_c("postCard", {
+  return _c("div", {
+    staticClass: "d-flex justify-content-center"
+  }, [_c("postCard", {
     key: _vm.post.id,
     attrs: {
       post: _vm.post
